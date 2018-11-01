e07fb3794e0ced56338daeb7aafea57059cd0959
U-DESKTOP-4LMFDCK\rpbas <rpbaskin@gmail.com>
2018-11-01 14:52:47 -0600

    A few bug fixes and testing info:`

--- db_cleaner.py
+++ db_cleaner.py
@@ -9,7 +9,7 @@ COPYRIGHT (c) R Paul Baskin 28 September 2018
 @author: rpbas
 """
 import os.path
-
+import sys
 
 
 # # # # # # # # #
@@ -73,155 +73,158 @@ def dup_remove(headers, duplicate):
     #                                 MAIN BODY
     #
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+if len(sys.argv) == 2:
+    try:
+        file_name = sys.argv[1]
+        my_file = open(file_name).read()
+        outputname = 'clean_' + file_name
+        OUT = open(outputname, 'w')
+    except:
+        print("filename doesn't exist")
     
-try:
-    file_name = input("please enter file name>>")
-    my_file = open(file_name).read()
-    outputname = 'clean_' + file_name
-    OUT = open(outputname, 'w')
-except:
-    print("file name doesn't exist")
-
-#starts to chop up the input FASTA file for analysis
-#breaks the long string file into a list  
-data = my_file.split('\n')
-data.pop(-1)
-#
-#
-#     THIS IS THE IMPORTANT PART
-#     if you want to change a specific character in the data set
-#
-i = 0
-for each in data:
-    data[i] = each.upper()
-    i += 1
-
-cleanupstring(data,' ', '_')
-cleanupstring(data,'\t', '_')
-cleanupstring(data,',', '_')
-cleanupstring(data, '"', '_')
-cleanupstring(data, '*', '')
-#cleanupstring(data, '(', '_')
-#cleanupstring(data, ')', '_')
-cleanupstring(data, '[', '_')
-cleanupstring(data, ']', '_')
-cleanupstring(data, '|', '_')
-i=0
-for each in data:
-    if each == '':
-        data.pop(i)
-    i+=1
-
-headers = []
-aa = []
-switch = 0
-for each in data:
-    if switch == 0:
-        headers.append(each)
-        switch = 1
-    elif switch == 1:
-        aa.append(each)
-        switch = 0
-#zippers the two lists together
-headerset = []
-aaa = []
-dbdict = dup_remove(headers,aa)  
+    #starts to chop up the input FASTA file for analysis
+    #breaks the long string file into a list  
+    data = my_file.split('\n')
+    data.pop(-1)
+    #
+    #
+    #     THIS IS THE IMPORTANT PART
+    #     if you want to change a specific character in the data set
+    #
+    i = 0
+    for each in data:
+        data[i] = each.upper()
+        i += 1
     
-
-#Eliminates reducendies
-
-
-
-
-
-#Turns dictionary into a text file
-
-for key, value in dbdict.items():
-    OUT.write(key)
-    headerset.append(key)
-    OUT.write('\n')
-    OUT.write(value)
-    aaa.append(value)
-    OUT.write('\n')
-
-OUT.close()
-
-
-#Run the program through signalp
-try:
-    cmd = './signalp -t euk -f short -u .4 %s > OUT.signalp_out' %outputname
-    print('running signalp')
-    os.system(cmd)
-except:
-    print("Signalp in incorrect location")
-    path = input('input path into folder containing signalp (ex: /home/user/destop/path/to/folder) >>')
-    cmd = '%s/signalp -t euk -f short -u .4 %s > OUT.signalp_out' % (path, outputname)
-    print('rerunning signalp')
-    os.system(cmd)
-    
-my_fileXXX = open('OUT.signalp_out').read()
-signalp = []
-#breaks into each row of data
-step = my_fileXXX.split('\n')
-#changes list into matrix
-for each in step:
-    signalp.append(each.split())
-
-#removes extra data
-signalp.remove(signalp[0])
-signalp.remove(signalp[0])    
-signalp.pop(-1)
-#assigns values of restriction site and if it occurs
-loc = []
-yes = []
-try:    
-    for each in signalp:
-        loc.append(each[4])
-        yes.append(each[9])
-except:
-    print('thinking')
-    
-print(len(aaa))
-
-try:
+    cleanupstring(data,' ', '_')
+    cleanupstring(data,'\t', '_')
+    cleanupstring(data,',', '_')
+    cleanupstring(data, '"', '_')
+    cleanupstring(data, '*', '')
+    #cleanupstring(data, '(', '_')
+    #cleanupstring(data, ')', '_')
+    cleanupstring(data, '[', '_')
+    cleanupstring(data, ']', '_')
+    cleanupstring(data, '|', '_')
     i=0
-    for each in aaa:
-        #searches the signalp data, if there is a signal peptide, then it runs this block
-        if yes[i] == 'Y':
-            #finds and masks the signal sequence
-                signal_sequence = each[0:(int(loc[i])-1)]
-                masked_signal_sequence = signal_sequence.lower()
-                aaa[i] = each.replace(signal_sequence, masked_signal_sequence)    
+    for each in data:
+        if each == '':
+            data.pop(i)
         i+=1
-except:
-    print('Error, probable cause that signalp ran incorrectly due to similar ' + '\n' + 'header names, contact Paul Baskin')
-
-#zippers the two lists together
-dbdict2 = dict(zip(headerset,aaa))
-
-
-
-
-
-#modifies the text file
-OUT = open(outputname, 'w')
-for key, value in dbdict2.items():
-    OUT.write(key)
-    OUT.write('\n')
-    chopper(value, 80, 160)
-OUT.close()
-
-#TODO
-# =============================================================================
-#     #Folds the program.
-#     cmd = 'fold outputname'
-#     print(cmd)
-#     os.system(cmd)
-#     
-# =============================================================================
-print('File clean_' + file_name + ' created.' )
-# =============================================================================
-# except:
-#     print("RAYOS! Something went wrong. Speak with Paul Baskin")
-#     quit()
-# =============================================================================
+    
+    headers = []
+    aa = []
+    switch = 0
+    for each in data:
+        if switch == 0:
+            headers.append(each)
+            switch = 1
+        elif switch == 1:
+            aa.append(each)
+            switch = 0
+    #zippers the two lists together
+    headerset = []
+    aaa = []
+    dbdict = dup_remove(headers,aa)  
+        
+    
+    #Eliminates reducendies
+    
+    
+    
+    
+    
+    #Turns dictionary into a text file
+    
+    for key, value in dbdict.items():
+        OUT.write(key)
+        headerset.append(key)
+        OUT.write('\n')
+        OUT.write(value)
+        aaa.append(value)
+        OUT.write('\n')
+    
+    OUT.close()
+    
+    
+    #Run the program through signalp
+    try:
+        cmd = './signalp -t euk -f short -u .4 %s > OUT.signalp_out' %outputname
+        print('running signalp')
+        os.system(cmd)
+    except:
+        print("Signalp in incorrect location")
+        path = input('input path into folder containing signalp (ex: /home/user/destop/path/to/folder) >>')
+        cmd = '%s/signalp -t euk -f short -u .4 %s > OUT.signalp_out' % (path, outputname)
+        print('rerunning signalp')
+        os.system(cmd)
+        
+    my_fileXXX = open('OUT.signalp_out').read()
+    signalp = []
+    #breaks into each row of data
+    step = my_fileXXX.split('\n')
+    #changes list into matrix
+    for each in step:
+        signalp.append(each.split())
+    
+    #removes extra data
+    signalp.remove(signalp[0])
+    signalp.remove(signalp[0])    
+    signalp.pop(-1)
+    #assigns values of restriction site and if it occurs
+    loc = []
+    yes = []
+    try:    
+        for each in signalp:
+            loc.append(each[4])
+            yes.append(each[9])
+            
+        print(loc)
+        print(yes)
+    except:
+        print('thinking')
+        
+    print(len(aaa))
+    
+    try:
+        i=0
+        for each in aaa:
+            #searches the signalp data, if there is a signal peptide, then it runs this block
+            if yes[i] == 'Y':
+                #finds and masks the signal sequence
+                    signal_sequence = each[0:(int(loc[i])-1)]
+                    masked_signal_sequence = signal_sequence.lower()
+                    aaa[i] = each.replace(signal_sequence, masked_signal_sequence)    
+            i+=1
+    except:
+        print('Error, probable cause that signalp ran incorrectly due to similar ' + '\n' + 'header names, contact Paul Baskin')
+    
+    #zippers the two lists together
+    dbdict2 = dict(zip(headerset,aaa))
+    
+    
+    
+    
+    
+    #modifies the text file
+    OUT = open(outputname, 'w')
+    for key, value in dbdict2.items():
+        OUT.write(key)
+        OUT.write('\n')
+        chopper(value, 80, 160)
+    OUT.close()
+    
+    #TODO
+    # =============================================================================
+    #     #Folds the program.
+    #     cmd = 'fold outputname'
+    #     print(cmd)
+    #     os.system(cmd)
+    #     
+    # =============================================================================
+    print('File clean_' + file_name + ' created.' )
+    # =============================================================================
+else:
+    print("Usage python3 db_cleaner.py path/to/filename")
