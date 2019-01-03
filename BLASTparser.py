c3a66a8213a47181a3cb5198a0a8064ce0105049
rpbaskin <rpbaskin@gmail.com>
2019-01-03 09:10:46 -0700

    A few bug fixes

--- BLASTparser.py
+++ BLASTparser.py
@@ -50,26 +50,32 @@ def getSUPFAM10(string):
     
     supfams = []
     text = my_file.split('\n')
-    count = round(text/10)
-    for each in text[0:count]:
-        if each.find('# >') != -1:
-            indexstart = each.find('SUPFAM:')
-            indexend = each.find('TPM:')
-            supfam = each[indexstart+7:indexend-1]
-            supfams.append(supfam)
+    count = round((getTotalHits(my_file))/10)
+    i=0
+    for each in text:
+        if i <= count:
+            if each.find('# >') != -1:
+                indexstart = each.find('SUPFAM:')
+                indexend = each.find('TPM:')
+                supfam = each[indexstart+7:indexend-1]
+                supfams.append(supfam)
+                i+=1
     return supfams
 
 def getSUPFAM25(string):
     
     supfams = []
     text = my_file.split('\n')
-    count = round(text/4)
-    for each in text[0:count]:
-        if each.find('# >') != -1:
-            indexstart = each.find('SUPFAM:')
-            indexend = each.find('TPM:')
-            supfam = each[indexstart+7:indexend-1]
-            supfams.append(supfam)
+    count = round((getTotalHits(my_file))/4)
+    i=0
+    for each in text:
+        if i<= count:
+            if each.find('# >') != -1:
+                indexstart = each.find('SUPFAM:')
+                indexend = each.find('TPM:')
+                supfam = each[indexstart+7:indexend-1]
+                supfams.append(supfam)
+                i+=1
     return supfams
 
     
@@ -93,7 +99,10 @@ if len(sys.argv) == 3:
     
     
     
-    
+    count10 = round((getTotalHits(my_file))/10)
+    count25 = round((getTotalHits(my_file))/25)
+    print(count10)
+    print(count25)
     
     
     totalhits = getTotalHits(my_file)
@@ -105,8 +114,8 @@ if len(sys.argv) == 3:
     
     OUT.write("file input: " + file_name + '\n' + "database type: " + sys.argv[2] + '\n' + "total hits: " + str(totalhits) +',,,top 25%,,,top 10%\n')
     OUT.write("SupFam,num_hits,percent_hit\n")
-    for a,b,c in itertools.izip(temp,temp25,temp10):
-        OUT.write(str(a)[1:-1] + ',' + str(round(a[1]/totalhits*100,2))+'%' + ',' + str(b)[1:-1] + ',' +str(round(b[1]/totalhits*25,2))+'%' + ',' + str(c)[1:-1] + ',' + str(round(a[1]/totalhits*10,2))+'%')
+    for a,b,c in zip(temp,temp25,temp10):
+        OUT.write(str(a)[1:-1] + ',' + str(round(a[1]/totalhits*100,2))+'%' + ',' + str(b)[1:-1] + ',' +str(round(b[1]/totalhits*100*4*1.1848,2))+'%' + ',' + str(c)[1:-1] + ',' + str(round(c[1]/totalhits*100*10,2))+'%')
         OUT.write('\n')
     
               
